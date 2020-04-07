c179e170305f4b865713d18c062127ee1e202da8
Samuel Gfeller <samuelgfeller@bluewin.ch>
2020-04-07 11:20:19 +0200

    Updated domain names in frontend config

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 var config = {
     // Url with ending slash
-    'api_url': 'http://dev.slim-api-example/',
-    'frontend_url': 'http://dev.frontend-example/',
+    'api_url': 'http://dev.time-sheet-api/',
+    'frontend_url': 'http://dev.time-sheet-frontend/',
 };
