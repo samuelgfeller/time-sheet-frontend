13112a0319fd99a96c9750fe1a88bfa7d8ffa7ce
Samuel Gfeller <samuelgfeller@bluewin.ch>
2020-04-08 11:16:38 +0200

    Wrote time difference calculation logic

--- js/timetracker.js
+++ js/timetracker.js
@@ -21,5 +21,28 @@ $(document).ready(function () {
         });
     });
 
+    let date1 = new Date("2020-03-29 08:00:00");
+    let date2 = new Date("2020-03-29 09:01:20");
 
+    // Calculating the difference between two dates. Result are Milliseconds
+    let diffInMs = date2.getTime() - date1.getTime();
+    // Convert ms to seconds
+    let diffInSeconds = diffInMs / 1000 ;
+    // Converting seconds to minutes
+    let diffInMin = diffInSeconds / 60;
+    // Calculating hours
+    let h = Math.floor(diffInMin / 60);
+    // Calculating minutes that can't be hours. Modulo returns the remainder after number is divided so the maximal dividable by 60 is in hours and the remainder will
+    // be caught by the modulo operator. If the diff in min is 125 -> 120 min is dividable by 60 and it gives 2(h) but since its 125 there is a remainder of
+    // 5min which the modulo exactly highlights. Math.floor is removing the decimals only keeping the integer
+    let m = Math.floor(diffInMin % 60);
+    // The seconds remainder in regards to the next 60 which are the minutes can be calculated with the same principle with the modulo operator
+    let s = diffInSeconds % 60;
+
+    $('#testingContainer').html('Total number of Hours between dates  <br>'
+        + date1 + '<br> and <br>'
+        + date2 + ' is: <br> '
+        + h + ':' + m
+        + ':' + s+'<br>'
+    );
 });
